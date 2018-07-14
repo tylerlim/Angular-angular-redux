6dd94d4edbbfcb586a955b66d965dfb7d5b0917f
Tyler <tylerjsc@gmail.com>
2018-07-14 10:27:22 +0800

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgReduxModule, NgRedux } from '@angular-redux/store'; // <- New
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgReduxModule, // <- New
   ],
   providers: [],
   bootstrap: [AppComponent]
