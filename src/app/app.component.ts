b29c91e384993e23dc840ff99fca4818368a343a
Tyler <tylerjsc@gmail.com>
2018-07-16 01:18:07 +0800

    add Destroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 
+import { NgRedux } from '@angular-redux/store'; // <- New
+import { CounterActions } from './app.actions';
+import {IAppState } from '../store';
+
+import { OnDestroy } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app works!';
   count: number; // <- New
+  subscription;
 
-  increment() {} // <- New
-  decrement() {} // <- New
+  constructor (
+    private ngRedux: NgRedux<IAppState>,
+    private actions: CounterActions) {
+      this.subscription = ngRedux.select<number>('count')
+      .subscribe(newCount => this.count = newCount);
+    }
+
+    ngOnDestroy() {
+      this.subscription.unsubscribe();
+    }
+
+  increment() {
+    this.ngRedux.dispatch(this.actions.increment());
+  } // <- New
+  decrement() {
+    this.ngRedux.dispatch(this.actions.decrement());
+  } // <- New
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgReduxModule, NgRedux } from '@angular-redux/store'; // <- New
 
+import { rootReducer, IAppState, INITIAL_STATE } from '../store';
+import { CounterActions } from './app.actions'; // <- New
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,18 @@ import { NgReduxModule, NgRedux } from '@angular-redux/store'; // <- New
     BrowserModule,
     NgReduxModule, // <- New
   ],
-  providers: [],
+  providers: [CounterActions], // <- New
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>) {
+     // Tell @angular-redux/store about our rootReducer and our initial state.
+    // It will use this to create a redux store for us and wire up all the
+    // events.
+    ngRedux.configureStore(
+      rootReducer,
+      INITIAL_STATE
+    );
+  }
+}
+
